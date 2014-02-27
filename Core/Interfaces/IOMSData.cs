a4d5820f0c74cc3feafcc345f5086306931e9a41
Iago Moreira da Silva <iago.silva@al.infnet.edu.br>
2014-02-27 15:08:03 -0300

    Upload da arqitetura inicial
    #1

---
+++ Core/Interfaces/IOMSData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Vtex.Billing.Core.Interfaces
+{
+    public interface IOMSData
+    {
+        decimal Value { get; }
+        Guid AccountId { get; }
+        string AccountName { get; }
+    }
+}
